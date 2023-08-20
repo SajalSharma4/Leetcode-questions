328f188a969d5a5316862b26ccd054101c30f48a
SajalSharma4 <sazalsharma2003@gmail.com>
2023-08-20 16:22:05 +0530

    Space Optimized 62. Unique Paths

--- 62. Unique Paths.java
+++ 62. Unique Paths.java
@@ -20,18 +20,16 @@ Explanation: From the top-left corner, there are a total of 3 ways to reach the 
 
 class Solution {
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
+        int[] dp = new int[n];
+        Arrays.fill(dp, 0);
         for (int i = 0; i < m; i++) {
-            dp[i][0] = 1;
-        }
-        for (int i = 0; i < n; i++) {
-            dp[0][i] = 1;
-        }
-        for (int i = 1; i < m; i++) {
+            int[] temp = new int[n];
+            temp[0] = 1;
             for (int j = 1; j < n; j++) {
-                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                temp[j] = dp[j] + temp[j - 1];
             }
+            dp = temp;
         }
-        return dp[m - 1][n - 1];
+        return dp[n - 1];
     }
 }
